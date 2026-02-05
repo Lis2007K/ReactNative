52ba936257dc6bb760ec78810ba4046f89164591
Lis2007K <lhaxhidauti@gmail.com>
2026-02-05 20:47:03 +0100

    Lesson 2

---
+++ lesson2/screens/ExerciseScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ExerciseScreen = () => {
+    let message = "Hi there"
+    message = "I am learnign code"
+    return(
+        <Text>{message}</Text>
+    )
+}
+
+export default ExerciseScreen;
