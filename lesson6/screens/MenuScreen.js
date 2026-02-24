aef4f62c35a09bf6cae2ccb0fda1de8d887fc226
Lis2007K <lhaxhidauti@gmail.com>
2026-02-24 20:43:09 +0100

    lesson 6

---
+++ lesson6/screens/MenuScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native';
+
+const MenuScreen = () => {
+    return(
+        <View>
+            <Text style={StyleSheet.text}>
+                Welcom to Menu Screen
+            </Text>
+            <Button
+                title="Go to List Screen"
+                color="purple"
+                onPress={() => console.log('Button Clicked')}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        textAlign: 'center',
+        marginVertical: 20,
+        fontSize: 25,
+    },
+});
+
+export default MenuScreen;
